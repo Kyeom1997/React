b15249c964be092685d4674d8690146c69c691ce
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-05-05 00:25:29 +0900

    .

---
+++ blog/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let posts = '강남 고기 맛집';
+
+  return ( 
+  <div className = "App">
+    <div className="black-nav">
+      <div>개발 Blog</div>
+    </div>
+    <img src= {logo}/>
+    <h4> {posts} </h4>
+  </div>
+  );
+}
+
+export default App;
